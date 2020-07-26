706ed2c73d4b46ed65d9df1b06c34e577799cd85
Matt Lee <poream3387@gmail.com>
2020-07-26 19:30:51 +0900

    Basic structure built

---
+++ playground.py
@@ -0,0 +1,11 @@
+import testoy.test_sort
+
+def bubbleSort(arr): 
+    n = len(arr)
+    for i in range(n): 
+        for j in range(0, n-i-1): 
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
+
+
+
---
+++ testoy/test_sort.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_sorting(, is_in_place):
+    pytest.main(["-v"])
+
+def test_null_case():
+    assert 10 > 1
+
+def test_one_element():
+    assert 1 > 10
+
+def test_few_elements():
+    pass
+
+def test_multiple_elements():
+    pass
