6d07965f2e7252c211d3d23982be895581256e0f
poream3387 <poream3387@gmail.com>
2020-07-27 23:48:43 +0900

    Implemented decorator_sort() and fixture function for getting the sort function user have used.

--- playground.py
+++ playground.py
@@ -1,11 +1,13 @@
-import testoy.test_sort
+from testoy.test_sort import decorator_sort
 
-def bubbleSort(arr): 
+# The sorting array has to take only an array and have to return the sorted array.
+@decorator_sort
+def bubbleSort(arr):
     n = len(arr)
-    for i in range(n): 
-        for j in range(0, n-i-1): 
-            if arr[j] > arr[j+1] : 
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]: 
                 arr[j], arr[j+1] = arr[j+1], arr[j] 
+    return arr
 
-
-
+bubbleSort([7, 1, 2, 3, 4, 5])
--- testoy/test_sort.py
+++ testoy/test_sort.py
@@ -1,16 +1,42 @@
 import pytest
 
-def test_sorting(, is_in_place):
-    pytest.main(["-v"])
+SORT_FUNC = {}
 
-def test_null_case():
-    assert 10 > 1
+edge_cases = [([], [])]
+small_cases = [([1], [1]), ([2, 1], [1, 2])] # length 1 ~ 4
+mid_cases = [([5, 4, 3, 2, 1], [1, 2, 3, 4, 5])] # length 5 ~ 100
+# large_cases = [([], [])] # length 100 ~
 
-def test_one_element():
-    assert 1 > 10
+# Will be used as a Decorator for users 
+# --> Saves the function to SORT_FUNC variable and execute pytesting.
+def decorator_sort(f):
+    global SORT_FUNC
+    SORT_FUNC[f.__name__] = f
+    pytest.main(["-v"]) # Execute: >pytest -v
+    return f
 
-def test_few_elements():
-    pass
+# Fixture that will be used for testing functions
+# --> This fixture gives test functions the sorting function we were to test with
+@pytest.fixture
+def fixture_sort():
+    return list(SORT_FUNC.values())[0]
 
-def test_multiple_elements():
-    pass
+# Test Case for Edge cases
+@pytest.mark.parametrize("test_case, sorted", edge_cases)
+def test_edge_cases(fixture_sort, test_case, sorted):
+    assert fixture_sort(test_case) == sorted 
+
+# Test Case for small test cases
+@pytest.mark.parametrize("test_case, sorted", small_cases)
+def test_small_cases(fixture_sort, test_case, sorted):
+    assert fixture_sort(test_case) == sorted 
+    
+# Test Case for mid-sized test cases
+@pytest.mark.parametrize("test_case, sorted", mid_cases)
+def test_mid_cases(fixture_sort, test_case, sorted):
+    assert fixture_sort(test_case) == sorted 
+
+# Test Case for large-sized test cases
+@pytest.mark.parametrize("test_case, sorted", large_cases)
+def test_large_cases(fixture_sort, test_case, sorted):
+    assert fixture_sort(test_case) == sorted 
