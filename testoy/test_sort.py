70f83c18508acf629ca96d9db3016da6f659f9e5
poream3387 <poream3387@gmail.com>
2020-07-28 23:27:18 +0900

    Implemented build_sort_case function / Having a prob with importing

--- testoy/test_sort.py
+++ testoy/test_sort.py
@@ -1,11 +1,12 @@
 import pytest
+from build_sort_case import build_sort_case
 
 SORT_FUNC = {}
 
 edge_cases = [([], [])]
-small_cases = [([1], [1]), ([2, 1], [1, 2])] # length 1 ~ 4
-mid_cases = [([5, 4, 3, 2, 1], [1, 2, 3, 4, 5])] # length 5 ~ 100
-# large_cases = [([], [])] # length 100 ~
+small_cases = build_sort_case(-1000, 1000, 5, 10) # length 1 ~ 4
+mid_cases = build_sort_case(-1000, 1000, 100, 10) # length 5 ~ 100
+large_cases =  build_sort_case(-1000, 1000, 100, 20) # length 100 ~ 
 
 # Will be used as a Decorator for users 
 # --> Saves the function to SORT_FUNC variable and execute pytesting.
@@ -16,7 +17,7 @@ def decorator_sort(f):
     return f
 
 # Fixture that will be used for testing functions
-# --> This fixture gives test functions the sorting function we were to test with
+# --> This fixture returns the sorting function user has put
 @pytest.fixture
 def fixture_sort():
     return list(SORT_FUNC.values())[0]
@@ -24,7 +25,7 @@ def fixture_sort():
 # Test Case for Edge cases
 @pytest.mark.parametrize("test_case, sorted", edge_cases)
 def test_edge_cases(fixture_sort, test_case, sorted):
-    assert fixture_sort(test_case) == sorted 
+    assert fixture_sort(test_case) == sorted
 
 # Test Case for small test cases
 @pytest.mark.parametrize("test_case, sorted", small_cases)
